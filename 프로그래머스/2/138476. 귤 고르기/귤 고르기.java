816f5fb54630397c1c941104162e128d5a4add83
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-03-20 14:09:11 +0900

    [level 2] Title: 귤 고르기, Time: 80.65 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,37 +2,27 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Comparator;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         
         int answer = 0;
         
-        
         Map<Integer, Integer> tangCntMap = new HashMap<>();
+        // 귤 크기에 따른 개수 맵에 저장
         for (int i = 0; i < tangerine.length; i++) {
             int cnt = tangCntMap.getOrDefault(tangerine[i], 0);
             tangCntMap.put(tangerine[i], cnt + 1);
         }
         
+        // 귤 개수 기준으로 내림차순 정렬
         List<Integer> tangKeys = new ArrayList<>(tangCntMap.keySet());
         
-        tangKeys.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer cnt1, Integer cnt2) {
-                return tangCntMap.get(cnt2) - tangCntMap.get(cnt1);
-            }
-        });
-            
-        int prevTang = 0;    
-            
+        tangKeys.sort((k1, k2) -> tangCntMap.get(k2) - tangCntMap.get(k1));
+
         for (Integer tang : tangKeys) {
             k -= tangCntMap.get(tang);
-            if (prevTang != tang) {
-                prevTang = tang;
-                answer++;
-            }
+            answer++;
             if (k <= 0) {
                 break;
             }            
