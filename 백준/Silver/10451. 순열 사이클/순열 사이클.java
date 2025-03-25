ae790c50d2b0e84df5081591f272ac737f624b85
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-03-25 09:30:30 +0900

    [Silver III] Title: 순열 사이클, Time: 524 ms, Memory: 74128 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -11,27 +11,36 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
+    // 테스트 케이스의 개수
     int T = Integer.parseInt(st.nextToken());
 
+    // 테스트 케이스의 개수만큼 테스트 배열 채우기
     for (int i = 0; i < T; i++) {
       st = new StringTokenizer(br.readLine());
       int N = Integer.parseInt(st.nextToken());
+      // 테스트 케이스 배열
       int[] testcase = new int[N];
+      // 방문 배열
       boolean[] visited = new boolean[N];
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < N; j++) {
         testcase[j] = Integer.parseInt(st.nextToken());
       }
+      // 사이클 탐색 시작
       search(testcase, visited);  
     }
   }
 
+  // 사이클 탐색
   private static void search(int[] testcase, boolean[] visited) {
 
+    // 순열 사이클 개수
     int cycleCnt = 0;
     for (int i = 1; i <= testcase.length; i++) {
+      // 아직 방문하지 않았을 경우
       if (!visited[i - 1]) {
         visited[i - 1] = true;
+        // 해당 노드에 대한 사이클 탐색 시작
         cycle(i, testcase, visited);
         cycleCnt++;
       }
@@ -41,6 +50,7 @@ public class Main {
 
   }
 
+  // 노드에 대한 사이클 탐색
   private static void cycle(int start, int[] testcase, boolean[] visited) {
     
     Queue<Integer> q = new LinkedList<>();
