87ce89eed3cbc12025e9b785aa273afaee4807ec
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-04-04 00:38:35 +0900

    update 과제 진행하기

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 class Solution {
+    
     public String[] solution(String[][] plans) {
         
         String[] answer = new String[plans.length];
@@ -11,59 +12,67 @@ class Solution {
         // 시작 시간 기준으로 오름차순 정렬 
         Arrays.sort(plans, (p1, p2) -> p1[1].compareTo(p2[1]));
         
-        // 과제 진행 큐
-        int maxTime = 0; // 제일 늦게 끝나는 과제 시간
-        Queue<Homework> queue = new LinkedList<>();        
-        for (int i = 0; i < plans.length; i++) {
-            int time = convertTimeStrToInt(plans[i][1]);
-            queue.offer(new Homework(plans[i][0], time, Integer.parseInt(plans[i][2])));            
-            maxTime = Math.max(maxTime, time);
+        // 과제 진행 큐        
+        Queue<Homework> queue = new LinkedList<>();
+        for (int i = 0; i < plans.length; i++) {            
+            queue.offer(convertArrToHomework(plans[i]));            
         }
         
         int idx = 0;
         Stack<Homework> leftHomework = new Stack<>();
-        while (idx < plans.length) {
-            Homework current = queue.poll();    
-            // 과제 진행 한바뀌 끝났다면
-            if (current.start == maxTime) {                
-                answer[idx++] = current.name;                
-                while (!leftHomework.isEmpty()) {
-                    answer[idx++] = leftHomework.pop().name;
-                }
+        
+        while (!queue.isEmpty()) {
+            Homework current = queue.poll();
+            // 남은 과제가 없을 경우
+            if (queue.peek() == null) {
+                answer[idx++] = current.name;
+                break;
             }
-            if (queue.peek() != null) {
-                int endTime = current.start + current.playtime;
-                // 과제 종료 시간이 다음 과제 시작시간보다 크다면
-                if (queue.peek().start < endTime) {
-                    int leftPlaytime = endTime - queue.peek().start;
-                    System.out.println(leftPlaytime);
-                    // 지연 과제 스택에 push
-                    leftHomework.push(new Homework(current.name, current.start, leftPlaytime));
-                } else {
-                    answer[idx] = current.name;
-                    idx++;
-                    // 과제 종료 후에도 시간이 남는다면
-                    if (queue.peek().start > endTime && !leftHomework.isEmpty()) {
-                        int leftTime = queue.peek().start - endTime;
-                        while (leftHomework.size() > 0 && leftTime > 0) {
-                            Homework left = leftHomework.pop();
-                            if (leftTime - left.playtime >= 0) {
-                                leftTime -= left.playtime;
-                                answer[idx++] = left.name;
-                            } else {
-                                left.playtime -= leftTime;                                
-                                leftHomework.push(left);
-                                leftTime = 0;
-                            }
+            // 종료 시간
+            int endTime = current.start + current.playtime;
+            // 과제 종료 시간이 다음 과제 시작시간보다 크다면
+            if (queue.peek().start < endTime) {
+                int leftPlaytime = endTime - queue.peek().start;
+                // 지연 과제 스택에 push                
+                leftHomework.push(new Homework(current.name, current.start, leftPlaytime));
+            } else {
+                answer[idx++] = current.name;                
+                // 과제 종료 후에도 시간이 남는다면
+                if (queue.peek().start > endTime && !leftHomework.isEmpty()) {
+                    // 남는 시간
+                    int leftTime = queue.peek().start - endTime;
+                    while (leftHomework.size() > 0 && leftTime > 0) {
+                        Homework left = leftHomework.pop();
+                        // 남는 시간 안에 과제를 종료할 수 있다면
+                        if (leftTime - left.playtime >= 0) {
+                            leftTime -= left.playtime;
+                            answer[idx++] = left.name;
+                        } else {
+                            left.playtime -= leftTime;                                
+                            leftHomework.push(left);
+                            leftTime = 0;
                         }
                     }
                 }
-            }
+            }            
         }
         
+        // 남은 숙제 순차적으로 꺼내서 종료 처리
+        while (!leftHomework.isEmpty()) {
+            answer[idx++] = leftHomework.pop().name;           
+        }
+
         return answer;
     }
     
+    // 배열을 Homework로 변환
+    private static Homework convertArrToHomework(String[] plan) {
+        int time = convertTimeStrToInt(plan[1]);
+        int playtime = Integer.parseInt(plan[2]);
+        return new Homework(plan[0], time, playtime);
+    }    
+    
+    // 시간 문자열 정수로 변환
     private static int convertTimeStrToInt(String time) {        
         String[] timeArr = time.split(":");
         return (Integer.parseInt(timeArr[0]) * 60) + Integer.parseInt(timeArr[1]);
@@ -72,12 +81,14 @@ class Solution {
 
 class Homework {
     
+    // 과제 명
     String name;
     
+    // 과제 시작시간
     int start;
     
+    // 과제 소요시간
     int playtime;
-    
     
     public Homework(String name, int start, int playtime) {
         this.name = name;
@@ -85,8 +96,4 @@ class Homework {
         this.playtime = playtime;
     }
     
-    @Override
-    public String toString() {
-        return String.format("%s 과제는 %d 시간에 시작하며 %d분 걸린다.", this.name, this.start, this.playtime);
-    }
 }
