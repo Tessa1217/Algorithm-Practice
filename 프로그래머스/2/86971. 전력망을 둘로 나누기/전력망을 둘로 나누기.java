5742ae287bca6590b5732b3520e4e8a1b3f58497
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-04-23 02:36:55 +0900

    Update 전력망을 둘로 나누기.java

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,23 +2,30 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     
     public int solution(int n, int[][] wires) {
+        
         int answer = wires.length;
-        List<List<Integer>> wireTree = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            wireTree.add(new ArrayList<>());
-        }
-        for (int i = 0; i < wires.length; i++) {            
-            wireTree.get(wires[i][0]).add(wires[i][1]);
-            wireTree.get(wires[i][1]).add(wires[i][0]);
+        
+        // 전력망 구조 세팅
+        
+        List<List<Integer>> wireTree = IntStream.rangeClosed(0, n)
+                                                .mapToObj(i -> new ArrayList<Integer>())
+                                                .collect(Collectors.toList());
+
+        for (int[] wire : wires) {            
+            wireTree.get(wire[0]).add(wire[1]);
+            wireTree.get(wire[1]).add(wire[0]);
         }
         
-        for (int i = 0; i < wires.length; i++) {
+        // 전선 연결에 따라 나누어진 두 전력망 각각의 송전탑 개수 계산
+        for (int[] wire : wires) {
             boolean[] visited = new boolean[n + 1];
-            int network1 = bfs(wires[i][0], wires[i][1], wireTree, visited);            
+            int network1 = bfs(wire[0], wire[1], wireTree, visited);            
             int network2 = n - network1;
             answer = Math.min(answer, Math.abs(network1 - network2));            
         }
@@ -37,9 +44,11 @@ class Solution {
         visited[connectWire] = true;
         
         while (!queue.isEmpty()) {
-            int wire = queue.poll();
-            List<Integer> wireList = wireTree.get(wire);
-            for (int next : wireList) {
+            
+            int current = queue.poll();
+            
+            for (int next : wireTree.get(current)) {
+                // 방문 처리 되지 않았으면서 끊으려는 전선이 아닌 경우
                 if (!visited[next] && next != cutOffWire) {
                     visited[next] = true;
                     queue.offer(next);
