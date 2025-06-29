2e2729fc58d9e3e58b84d2f80ad359a381ab10a0
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-06-30 02:38:30 +0900

    [level 2] Title: 가장 큰 수, Time: 163.80 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    const answer = numbers.map(number => String(number))
+                          .sort((a, b) => (b + a) - (a + b))
+                          .join('');
+    return answer[0] === '0' ? '0' : answer;
+}
