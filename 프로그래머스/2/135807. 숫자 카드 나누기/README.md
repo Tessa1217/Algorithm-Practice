287a0ddec10451039c6411c208dacd19400eaafd
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-07-04 10:08:19 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 62.85 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/2/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 67.63 ms
+메모리: 79.2 MB, 시간: 62.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 04일 09:57:12
+2025년 07월 04일 10:08:41
 
 ### 문제 설명
 
--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,40 +1,33 @@
 // 한 사람이 가진 카드들에 모든 숫자를 나눌 수 있다 => 모든 수의 공약수
 // 다른 사람이 가진 카드들에 적힌 모든 숫자를 나눌 수 없다
-function solution(arrayA, arrayB) {
-    var answer = 0;
-    
-    const arrayADivisors = getDivisorsOfCards(arrayA)
-    const arrayBDivisors = getDivisorsOfCards(arrayB)
-    
-    
-    const arrayANonDivisor = getMaxNonDivisor(arrayA, arrayBDivisors)
-    const arrayBNonDivisor = getMaxNonDivisor(arrayB, arrayADivisors)    
+function solution(arrayA, arrayB) {    
+
+    const arrayANonDivisor = getMaxNonDivisor(arrayA, getDivisorsOfCards(arrayB))
+    const arrayBNonDivisor = getMaxNonDivisor(arrayB, getDivisorsOfCards(arrayA))    
     
     return Math.max(arrayANonDivisor, arrayBNonDivisor);
 }
 
 // 카드에 대해 조건을 만족하는 가장 큰 양의 정수 구하기
 function getMaxNonDivisor(array, divisors) {
-    let nonDivisor = 0
-    for (let i = 0; i < divisors.length; i++) {
-        let divisor = divisors[i];
-        if (divisor === 1) {
-            continue
-        }
-        for (let j = 0; j < array.length; j++) {
-            if (array[j] % divisor === 0) { // 나눌 수 있는 수라면 더 보지않고 break                
-                divisor = 0                
-                break                
-            }
-        }        
-        nonDivisor = Math.max(nonDivisor, divisor)        
-    }
-    return nonDivisor
+    
+    // 나눌 수 있는 수 판별                
+    const canDivide = (array, divisor) => array.find((element) => element % divisor === 0)
+    
+    return divisors.reduce((accum, divisor) => {
+        if (canDivide(array, divisor) === undefined) {
+            accum = Math.max(accum, divisor)
+        }    
+        return accum
+    }, 0)
+    
+    return maxNonDivisor
+    
 }
 
 // 숫자 카드 전체의 공약수 구하기
 function getDivisorsOfCards(array) {
-    const gcdOfArray = array.slice(1).reduce((a, b) => gcd(Math.min(a, b), Math.max(a, b)), array[0])
+    const gcdOfArray = array.reduce((a, b) => gcd(Math.min(a, b), Math.max(a, b)))
     return getDivisors(gcdOfArray)
 }
 
