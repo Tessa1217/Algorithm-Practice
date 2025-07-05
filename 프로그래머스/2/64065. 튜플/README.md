1da76c42dcb85cac13a8625a2498c39f0eca787f
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-07-05 22:32:51 +0900

    [level 2] Title: 튜플, Time: 78.67 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
 
 ### 성능 요약
 
-메모리: 51.6 MB, 시간: 52.67 ms
+메모리: 112 MB, 시간: 78.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 05일 22:14:38
+2025년 07월 05일 22:32:55
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+class Solution {
+    public Integer[] solution(String s) {        
+        
+        String[] parsedArr = s.substring(2, s.length() - 2).split("\\}\\,\\{");
+        
+        Arrays.sort(parsedArr, (a, b) -> a.length() - b.length());
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < parsedArr.length; i++) {
+            String[] arr = parsedArr[i].split(",");
+            for (int j = 0; j < arr.length; j++) {
+                Integer num = Integer.parseInt(arr[j]);                
+                if (answer.indexOf(num) > -1) {
+                    continue;
+                } 
+                answer.add(num);
+            }            
+        }
+            
+        return answer.toArray(new Integer[0]);
+    }
+}
