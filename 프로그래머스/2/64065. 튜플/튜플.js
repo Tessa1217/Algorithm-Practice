8a77d751b8e1a602b212f634dc63817ac3354b88
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-07-05 22:14:33 +0900

    [level 2] Title: 튜플, Time: 52.67 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -3,11 +3,10 @@ function solution(s) {
     // 튜플은 원소의 순서가 정해져 있음
     // 튜플의 원소 순서는 리스트에서 길이가 짧은 원소 -> 길이가 긴 원소로 파악 가능
     // 집합은 원소의 순서가 바뀌어도 상관이 없음    
-    const slist = s.split("{")
-                   .filter(e => e)
-                   .map(e => e.replaceAll("}", "").split(",").filter(e => e).map(e => parseInt(e)))
-                   .sort((a, b) => a.length - b.length)
-                   .flat(Infinity);           
-    return [...new Set(slist)];    
-    
+    return s.slice(2, s.length - 2)
+            .split("},{")
+            .map((num) => num.split(","))      
+            .sort((a, b) => a.length - b.length)
+            .reduce((accum, ele) => [...new Set([...accum, ...ele])])
+            .map((element) => parseInt(element))    
 }
