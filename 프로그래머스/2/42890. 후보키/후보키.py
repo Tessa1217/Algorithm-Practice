5a46a6a6cbdf9d2db8a5f10e57a2d7ad3c5556c2
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-07-28 12:44:38 +0900

    [level 2] Title: 후보키, Time: 3.97 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -1,34 +1,25 @@
 from itertools import combinations
 def solution(relation):
-    answer = 0    
-    key_length = len(relation[0])
-    arr = [i for i in range(key_length)]
-    key_comb = list(c for i in range(1, key_length + 1) for c in combinations(arr, i))
+    num_cols = len(relation[0])
+    all_col_indices = range(num_cols)
     
-    candidates = []
-    visited = []
-    for key in key_comb:
-        data_key_set = generate_based_on_candidates(key, relation)           
-        if len(data_key_set) == len(relation) and not check_if_key_exists(key, visited):            
-            candidates.append(key)
-            visited.append("".join(map(str, key)))            
-                                
-    return len(candidates)
+    all_combinations = [comb for i in range(1, num_cols + 1) for comb in combinations(all_col_indices, i)]
+    
+    candidate_keys = []
+    
+    for comb in all_combinations:
+        if is_unique(comb, relation) and is_minimal(comb, candidate_keys):
+            candidate_keys.append(set(comb))
+            
+    return len(candidate_keys)
 
-def generate_based_on_candidates(key, relation):
-    key_set = set()
-    for row in relation:
-        str = ''
-        for k in list(key):
-            str += row[k] + "&" # spliter
-        key_set.add(str)
-    return key_set
+def is_unique(comb, relation):
+    projected = {tuple(row[i] for i in comb) for row in relation}
+    return len(projected) == len(relation)
 
-# 최소성 만족을 위해 sub set인지 확인
-def check_if_key_exists(key, visited):
-    key_set = set(key)
-    for v in visited:        
-        v_set = set(map(int, v))        
-        if v_set.issubset(key_set):            
-            return True
-    return False
+def is_minimal(comb, candidate_keys):
+    comb_set = set(comb)
+    for key in candidate_keys:
+        if key.issubset(comb_set):
+            return False
+    return True
