104416e19e15590090ac0366194a7c8e8dbe4a42
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-08-02 21:12:36 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.37 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 33.82 ms
+메모리: 36.9 MB, 시간: 10.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 02일 21:10:47
+2025년 08월 02일 21:12:44
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,9 +1,10 @@
 function solution(n, k) {    
-    const converted = n.toString(k).split("0")
-    return converted.reduce((ans, val) => ans += is_prime(Number(val)) ? 1 : 0, 0);        
+    const converted = n.toString(k).split("0")    
+    return converted.filter((v) => v)
+                    .reduce((ans, val) => ans += is_prime(Number(val)) ? 1 : 0, 0);        
 }
 
-function is_prime(num) {
+function is_prime(num) {        
     if (num < 2) return false
     for (let i = 2; i * i <= num; i++) {
         if (num % i == 0) return false
