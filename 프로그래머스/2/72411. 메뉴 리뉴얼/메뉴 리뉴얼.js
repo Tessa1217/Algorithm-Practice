047b35e2f57362ba234e68eb73408c86704d9a4c
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-08-06 00:30:25 +0900

    update 메뉴 리뉴얼 add comment

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,28 +1,44 @@
 function solution(orders, course) {
     var answer = [];
-    const cnt_map = new Map(course.map((c) => [c, 0]))    
-    const menu_map = {}
+    
+    // 코스 매뉴 맵
+    const courseMenus = new Map()
+    // 맵의 형태 Map(조합길이(코스메뉴 길이), Map(조합문자열, 등장횟수))
     for (let c of course) {
-        orders.forEach((order) => {
-            const menu_order = order.split("")
-            menu_order.sort()
-            combination(menu_order, c).forEach((comb) => {
-                const comb_key = comb.join("")
-                menu_map[comb_key] = (menu_map[comb_key] || 0) + 1    
-                cnt_map.set(c, Math.max(cnt_map.get(c), menu_map[comb_key]))                
-            })
-        })
+        courseMenus.set(c, new Map())
     }
     
-    Object.entries(menu_map).forEach(([key, value]) => {        
-        if (value >= 2 && cnt_map.get(key.length) === value) {
-            answer.push(key)
+    for (const order of orders) {
+        // 원소에 저장된 문자열은 오름차순 정렬 필요
+        const sortOrder = order.split("").sort()
+        for (const c of course) {
+            // 조합의 길이가 코스 메뉴보다 짧다면 조합 만들지 못하므로
+            // continue 처리
+            if (sortOrder.length < c) continue;
+            for (const comb of combination(sortOrder, c)) {
+                const menu = comb.join("")
+                const menuMap = courseMenus.get(c)
+                menuMap.set(menu, (menuMap.get(menu) || 0) + 1)
+            }
         }
-    })
+    }
     
-    answer.sort()
-        
-    return answer;
+    for (const [length, menuMap] of courseMenus.entries()) {
+        // 코스 길이의 max 카운트
+        const maxCount = Math.max(...menuMap.values(), 0)
+        // 코스 요리는 2번 이상 주문되어야 구성 가능
+        if (maxCount < 2) {
+            continue
+        }
+        // 가장 많은 메뉴 구성은 여러 개일수 있음
+        for (const [menu, count] of menuMap.entries()) {
+            if (count === maxCount) {
+                answer.push(menu)
+            }
+        }
+    }
+    
+    return answer.sort();
 }
 
 function combination(arr, selectNumber) {
