14fd4382fa86ebf74bca447af1c3b709656b8b6e
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-08-09 00:45:28 +0900

    [level 0] Title: 특이한 정렬, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,8 @@
+function solution(numlist, n) {    
+    return numlist.sort((a, b) => {
+        if (Math.abs(a - n) === Math.abs(b - n)) {
+            return b - a
+        }
+        return Math.abs(a - n) - Math.abs(b - n)
+    })    
+}
